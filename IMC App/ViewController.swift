8dcae8f1c11a2195f5df513b32c794247f9cbfd6
Raul Medeiros <raul.medeiros@vortigo.com.br>
2022-11-09 16:04:44 -0300

    Initial Commit

---
+++ IMC App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMC App
+//
+//  Created by Raul de Medeiros on 09/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
